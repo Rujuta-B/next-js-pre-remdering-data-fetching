735a5acc3279d1cc76afd708c5484745e981723b
Rujuta Batavia <rujutambatavia@gmail.com>
2022-08-03 15:00:12 +0530

    loading paths dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -12,14 +12,18 @@ const ProductPage = (props) => {
   );
 };
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-data.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  const data = await getData();
   const product = data.products.find((product) => product.id === productId);
 
   return {
@@ -30,12 +34,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
+    paths: pathParams,
     fallback: "blocking",
   };
 }
