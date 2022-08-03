22fa96a2cedc347e75281fdb65ca78dc518ebb37
Rujuta Batavia <rujutambatavia@gmail.com>
2022-08-03 12:13:18 +0530

    getStaticProps, ISR, dynamic paths, getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import fs from "fs/promises";
 import path from "path";
+import Link from "next/link";
 
 function HomePage(props) {
   const { products } = props;
   return (
     <ol>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ol>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const filePath = path.join(process.cwd(), "data", "dummy-data.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
@@ -21,6 +24,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
